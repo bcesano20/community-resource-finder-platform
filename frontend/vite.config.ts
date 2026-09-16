3ecf6d57e734608ad759036d1f6bf499e1cb0da5
bcesano20 <54218077+bcesano20@users.noreply.github.com>
2026-09-16 09:52:47 +0200

    add config, linting and main global files

---
+++ frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import path from 'node:path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(import.meta.dirname, './src/components'),
+      '@/pages': path.resolve(import.meta.dirname, './src/pages'),
+      '@/hooks': path.resolve(import.meta.dirname, './src/hooks'),
+      '@/api': path.resolve(import.meta.dirname, './src/apiCalls'),
+      '@/types': path.resolve(import.meta.dirname, './src/types'),
+      '@/helpers': path.resolve(import.meta.dirname, './src/helpers'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+  },
+});
