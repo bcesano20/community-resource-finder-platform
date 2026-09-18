f7be4489506cb23e28dce2df60abe2d208f94edc
bcesano20 <54218077+bcesano20@users.noreply.github.com>
2026-09-18 15:13:22 +0200

    add models

---
+++ backend/core/models/__init__.py
@@ -0,0 +1,4 @@
+from core.models.category import Category
+from core.models.resource import Resource
+
+__all__ = ["Category", "Resource"]
---
+++ backend/core/models/category.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
